af2bfb7e47e21513c1ee1e1830214a417d8c3cbb
Jonathan Daugherty <cygnus@foobox.com>
2023-07-18 11:29:33 -0700

    First pass at multi-platform wrapper implementation

--- src/Graphics/Vty/CrossPlatform.hs
+++ src/Graphics/Vty/CrossPlatform.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE CPP #-}
 module Graphics.Vty.CrossPlatform
-  (
-  ) where
+  ( mkVty
+  )
+where
+
+import Graphics.Vty (Vty)
+import Graphics.Vty.Config (VtyUserConfig)
+
+#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+import qualified Graphics.Vty.Platform.Windows as Platform
+#else
+import qualified Graphics.Vty.Platform.Unix as Platform
+#endif
+
+mkVty :: VtyUserConfig -> IO Vty
+mkVty = Platform.mkVty
