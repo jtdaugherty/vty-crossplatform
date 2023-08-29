dfb2ed624f73df0144acda8e74b71fed497a3569
ShrykeWindgrace <shryke.windgrace@gmail.com>
2023-08-29 11:45:08 +0200

    Migrate interactive terminal test to crossplatform
    NB! It exposes a helper 'mkDefaultOutput', which is not intended for
    end-user purposes.

---
+++ src/Graphics/Vty/CrossPlatform/Testing.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE CPP #-}
+
+module Graphics.Vty.CrossPlatform.Testing (mkDefaultOutput) where
+
+import Graphics.Vty (Output)
+
+#if defined(mingw32_HOST_OS) || defined(__MINGW32__)
+import Graphics.Vty.Platform.Windows.Output (buildOutput)
+import Graphics.Vty.Platform.Windows.Settings (defaultSettings)
+#else
+import Graphics.Vty.Platform.Unix.Output (buildOutput)
+import Graphics.Vty.Platform.Unix.Settings (defaultSettings)
+#endif
+
+
+-- | This helper is not intended for end-user consumption; it is exposed only for testing purposes.
+mkDefaultOutput :: IO Output
+mkDefaultOutput = defaultSettings >>= buildOutput
